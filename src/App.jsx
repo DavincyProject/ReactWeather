3f035d729bc287194ef6c618a1e0e937d3203d5b
Davincy Project <bdavincy.bd@gmail.comm>
2023-11-16 16:51:53 +0700

    feature: add redux and error pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return (
-    <>
-      <h1 className="text-5xl text-blue-600">Hello World</h1>
-    </>
-  );
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+
+                    {/* handle route error */}
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    );
 }
 
 export default App;
